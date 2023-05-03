0071b7f59086d92a0c308a55df306d1f66a67ac3
Nicolaj Gajhede <ngajhede@gmail.com>
2023-05-03 08:23:39 +0200

    use nanoid for numnbers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { stringify } from "csv-stringify";
 import fs from "fs";
+import { customAlphabet } from "nanoid";
+const nanoid = customAlphabet("1234567890", 10);
 
 const numRows = 1000000; // Replace with desired number of rows
 const name = `./output/generated-contacts-${faker.datatype.string()}.csv`;
@@ -10,9 +12,7 @@ const records = [];
 for (let i = 0; i < numRows; i++) {
   const name = faker.name.fullName();
   const countryCode = 45;
-  const phoneNumber = faker.datatype.number({
-    min: 1000000,
-  });
+  const phoneNumber = nanoid();
   records.push({ name, countryCode, phoneNumber });
 }
 

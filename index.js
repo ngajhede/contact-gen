b62416a7fdddf0ff84ab90b35a304b3525b60cf6
Nicolaj Gajhede <ngajhede@gmail.com>
2023-08-11 13:25:33 +0200

    country codes

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import fs from "fs";
 import { customAlphabet } from "nanoid";
 const nanoid = customAlphabet("1234567890", 10);
 
-const numRows = 1000000; // Replace with desired number of rows
+const numRows = 3274; // Replace with desired number of rows
 const name = `./output/generated-contacts-${faker.datatype.string()}.csv`;
 
 const records = [];
 
+const countryCodes = [44, 45, 46, 47, 48, 49];
+
 for (let i = 0; i < numRows; i++) {
   const name = faker.name.fullName();
-  const countryCode = 45;
+  const countryCode = countryCodes[faker.datatype.number(5)];
   const phoneNumber = nanoid();
-  records.push({ name, countryCode, phoneNumber });
+  const hasTag = faker.datatype.boolean();
+  records.push({ name, countryCode, phoneNumber, hasTag });
 }
 
 stringify(
@@ -24,6 +27,7 @@ stringify(
       name: "Name",
       countryCode: "Country Code",
       phoneNumber: "Phone Number",
+      hasTag: "Tag",
     },
   },
   (err, output) => {
